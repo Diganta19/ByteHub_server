fd203f0085194949c70ce7dd7f37f247507dcd70
Diganta Nayak <diganta.19032001@gmail.com>
2024-01-01 18:07:35 +0530

    Fixed Bug

--- controllers/Subscription.js
+++ controllers/Subscription.js
@@ -31,8 +31,8 @@ export const AskSubscription = async(req,res)=>{
                 quantity:1
 
                 }],
-            success_url: `http://localhost:3000/success/${userId}?plan=${subscriptionPlan.name}`,
-            cancel_url: 'http://localhost:3000/',
+            success_url: `https://wondrous-halva-f4c999.netlify.app//success/${userId}?plan=${subscriptionPlan.name}`,
+            cancel_url: 'https://wondrous-halva-f4c999.netlify.app/',
         })
         
          res.json({url:session.url, planName:subscriptionPlan.name})
