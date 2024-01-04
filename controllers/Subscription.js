c778d983723e17c283ad1434ff3fda8436b15387
Diganta Nayak <diganta.19032001@gmail.com>
2024-01-04 11:28:12 +0530

    Completed.Changed payment gateway from stripe to razorpay

--- controllers/Subscription.js
+++ controllers/Subscription.js
@@ -1,12 +1,15 @@
-import Stripe from "stripe";
+import Razorpay from "razorpay";
 import User from "../models/auth.js"
 import mongoose from "mongoose";
+import 'dotenv/config'
+import crypto from "crypto"
 
 
-const stripe = new Stripe('sk_test_51OPSz1SDb36Igos31yuik3SQrEIDvJgtrBpSx9fpDNaPXXs894uEfs4Lfq0ZXmgqrlx4D1V1XcjhLWKqD9iQmphZ00k5tj7QBt');
+
+//const stripe = new Stripe('sk_test_51OPSz1SDb36Igos31yuik3SQrEIDvJgtrBpSx9fpDNaPXXs894uEfs4Lfq0ZXmgqrlx4D1V1XcjhLWKqD9iQmphZ00k5tj7QBt');
 
 const membership = new Map([
-    [1,{name: 'SILVER', priceInCents: 0}],
+    [1,{name: 'SILVER', priceInCents: 10}],
     [2,{name: 'GOLD', priceInCents: 50000}],
     [3,{name:  'PLATINUM', priceInCents: 100000}],
 ])
@@ -15,42 +18,86 @@ const membership = new Map([
 export const AskSubscription = async(req,res)=>{
     try {
          const {itemId,userId} = req.body;
-        console.log(userId);
         const subscriptionPlan = membership.get(JSON.parse(itemId))
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types:['card'],
-            mode:"payment",
-            line_items:[{
-                price_data:{
-                    currency:'INR',
-                    product_data:{
-                        name:subscriptionPlan.name
-                    },
-                    unit_amount:subscriptionPlan.priceInCents
-                },
-                quantity:1
-
-                }],
-            success_url: `/success/${userId}?plan=${subscriptionPlan.name}`,
-            cancel_url: 'https://wondrous-halva-f4c999.netlify.app/',
-        })
         
-         res.json({url:session.url, planName:subscriptionPlan.name})
+       
+
+        // const session = await stripe.checkout.sessions.create({
+        //     payment_method_types:['card'],
+        //     mode:"payment",
+        //     line_items:[{
+        //         price_data:{
+        //             currency:'INR',
+        //             product_data:{
+        //                 name:subscriptionPlan.name
+        //             },
+        //             unit_amount:subscriptionPlan.priceInCents
+        //         },
+        //         quantity:1
+
+        //         }],
+        //     success_url: `http://localhost:3000/success/${userId}?plan=${subscriptionPlan.name}`,
+        //     cancel_url: 'http://localhost:3000/',
+        // })
+        
+        //  res.json({session:session, url:session.url, userId:userId, planName:subscriptionPlan.name})
+        // res.json({session:session})
+
+        const instance = new Razorpay({
+            key_id:process.env.KEY_ID,
+            key_secret:process.env.KEY_SECRET
+            
+        });
+        
+        const options = {
+            amount:subscriptionPlan.priceInCents * 100,
+            currency:"INR",
+            // recipt: crypto.randomBytes(10).toString('hex'),
+        };
+
+        instance.orders.create(options,(error,order)=>{
+            if(error){
+                console.log(error);
+                return res.status(500).json({message:"SOMETHING WENT WRONG"});
+            }
+            res.status(200).json({data:order,subplan:subscriptionPlan.name,UId:userId})
+        });
     } catch (error) {
-        res.status(500).json({error: error.message})
+        console.log(error);
+        res.status(500).json(error)
+    }
+}
+
+export const verifyPayment = async(req,res)=>{
+    
+    try {
+        const {razorpay_order_id,razorpay_payment_id,razorpay_signature} =  req.body
+        console.log(razorpay_order_id,razorpay_payment_id,razorpay_signature);
+        const sign = razorpay_order_id +"|"+razorpay_payment_id;
+        const expectedSign = crypto.createHmac("sha256",process.env.KEY_SECRET).update(sign.toString()).digest("hex")
+
+        if(razorpay_signature === expectedSign){
+            res.status(200).json({message:"PAYMENT VERIFIED"})
+        }else{
+            res.status(500).json({message:"PAYMENT FAILED"})
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"INTERNAL SERVER ERROR"})
     }
 }
 
 export const updateSubscription = async(req,res) =>{
     const{id:_id} = req.params;
-    const {subscription} = req.body;
+    const {subplan} = req.body;
+    console.log("sub",subplan);
     
 
     if(!mongoose.Types.ObjectId.isValid(_id)){
         return res.status(404).send('User unavliabe....')
     }
     try {
-        const updatedSubscription = await User.findByIdAndUpdate(_id,{$set:{'subscription':subscription}},{new:true})
+        const updatedSubscription = await User.findByIdAndUpdate(_id,{$set:{'subscription':subplan}},{new:true})
         res.status(200).json(updatedSubscription)
     } catch (error) {
         res.status(505).json({error:error.message})
--- routes/Subscription.js
+++ routes/Subscription.js
@@ -1,10 +1,11 @@
 import  express from "express";
-import { AskSubscription,updateSubscription } from "../controllers/Subscription.js";
+import { AskSubscription,updateSubscription,verifyPayment } from "../controllers/Subscription.js";
 
 const router = express.Router();
 
 
 router.post('/create-checkout-session',AskSubscription)
+router.post('/verify',verifyPayment)
 router.patch(`/update/:id`,updateSubscription)
 
 export default router
